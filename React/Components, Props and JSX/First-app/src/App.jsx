8729183b48611cb359f27fd2b2a6cb83c8cb00f4
BikashPandeya <bikashpandeya5@gmail.com>
2025-02-05 09:14:17 +0545

    React

---
+++ React/Components, Props and JSX/First-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+      <div className="Harry"></div>
+    </>
+  )
+}
+
+export default App
