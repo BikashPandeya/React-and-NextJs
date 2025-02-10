e2d53f9706bee5dc4888e1c0c4c7ab542bca8f8f
BikashPandeya <bikashpandeya5@gmail.com>
2025-02-10 07:52:12 +0545

    React

--- React/09_TodoList_App/src/App.jsx
+++ React/09_TodoList_App/src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from "./Components/Navbar"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type="text" className='border-2 border-purple-600 w-1/2 '/>
+          <button className='bg-violet-800 hover:bg-violet-600 p-3 py-1 text-white rounded-md mx-6 font-bold'>Submit</button>
+        </div>
+        <div className="text-xl font-bold" >
+          <h2>Your Todos</h2>
+        </div>
+
+        <div className="Todos">
+          <div className="todo flex flex-col">
+            <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, aliquam!</div>
+            <div className="buttons">
+              <button className='bg-violet-800 hover:bg-violet-600 p-3 py-1 text-white rounded-md mx-6 font-bold'>Edit</button>
+              <button className='bg-violet-800 hover:bg-violet-600 p-3 py-1 text-white rounded-md mx-6 font-bold'>Delete</button>
+
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
