16ce2dca9b4fce94727d1c0dbd1df6d90feeb1c0
BikashPandeya <bikashpandeya5@gmail.com>
2025-02-25 22:50:54 +0545

    Use Context Hook

---
+++ React/12_Use_Context_Hook/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button'
+
+const Navbar = ({count}) => {
+  return (
+        // <div>
+        //   <div>Hii I am Navbar</div>
+        //     <Button count={count}/>
+        // </div>
+    <div>
+      <div>Hii I am Navbar</div>
+        <Button />
+    </div>
+  )
+}
+
+export default Navbar
